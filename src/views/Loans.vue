0e817e71f81db769edc27d5415469d24991752f8
M. Monster <horatius83@gmail.com>
2020-11-01 21:21:45 -0600

    Added fake loans

--- src/views/Loans.vue
+++ src/views/Loans.vue
@@ -1,3 +1,36 @@
 <template>
     <h1>Loans</h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Principal</th>
+            <th>Interest</th>
+            <th>Minimum</th>
+        </tr>
+        <tr v-for="(loan, index) in loans" :key="index">
+            <td>{{loan.name}}</td>
+            <td>{{loan.principal}}</td>
+            <td>{{loan.interest}}</td>
+            <td>{{loan.minimum}}</td>
+        </tr>
+    </table>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Loans',
+  components: {
+    // HelloWorld,
+  },
+  data() {
+      return {
+          loans: [
+            {name: 'Test Name', principal: 1983, interest: 0.05, minimum: 19.0},
+            {name: 'Test Name 2', principal: 1985, interest: 0.03, minimum: 19.0}
+          ]
+      }
+  }
+});
+</script>
