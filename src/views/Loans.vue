23d5fcc8e3311a46d44dab1d8a399a8a945746c8
M. Monster <horatius83@gmail.com>
2020-11-21 22:09:53 -0600

    Added some divs, trying to get references working

--- src/views/Loans.vue
+++ src/views/Loans.vue
@@ -1,37 +1,40 @@
 <template>
-    <h1>Loans</h1>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Principal</th>
-            <th>Interest</th>
-            <th>Minimum</th>
-            <th></th>
-        </tr>
-        <tr v-for="(loan, index) in loans" :key="index">
-            <td>{{ loan.name }}</td>
-            <td>{{ loan.principal }}</td>
-            <td>{{ loan.interest }}</td>
-            <td>{{ loan.minimum }}</td>
-            <td><button v-on:click="deleteLoan(index)">Delete</button></td>
-        </tr>
-    </table>
+    <div id="loans-table">
+        <h2>Loans</h2>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Principal</th>
+                <th>Interest</th>
+                <th>Minimum</th>
+                <th></th>
+            </tr>
+            <tr v-for="(loan, index) in loans" :key="index">
+                <td>{{ loan.name }}</td>
+                <td>{{ loan.principal }}</td>
+                <td>{{ loan.interest }}</td>
+                <td>{{ loan.minimum }}</td>
+                <td><button v-on:click="deleteLoan(index)">Delete</button></td>
+            </tr>
+        </table>
+    </div>
     <div class="add-loan">
+        <h2>Add Loan</h2>
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" v-model="name" >
+        <input type="text" name="name" id="name" ref="nameRef" v-model.trim="name" >
         <label for="principal">Principal</label>
-        <input type="number" name="principal" id="principal" v-model="principal" >
+        <input type="number" name="principal" id="principal" v-model.number="principal" >
         <label for="interest">Interest</label>
-        <input type="number" name="interest" id="interest" v-model="interest" >
+        <input type="number" name="interest" id="interest" v-model.number="interest" >
         <label for="minimum">Minimum</label>
-        <input type="number" name="minimum" id="minimum" v-model="minimum" >
+        <input type="number" name="minimum" id="minimum" v-model.number="minimum" >
         <button v-on:click="addLoan">Submit</button>
     </div>
 </template>
 <script lang="ts">
 import { Loan } from '@/models/loan';
 import { addLoan, deleteLoan, getLoans } from '@/store/loans.state';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'Loans',
@@ -42,7 +45,7 @@ export default defineComponent({
         name: '',
         principal: 0,
         interest: 0,
-        minimum: 0
+        minimum: 0,
       }
   },
   computed: {
